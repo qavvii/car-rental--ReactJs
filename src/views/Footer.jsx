ffa817344bcda91650ea591c2e87ab32fb7ad790
Qavi <136834246+qavvii@users.noreply.github.com>
2023-12-18 22:43:34 +0530

    Add files via upload

---
+++ src/views/Footer.jsx
@@ -0,0 +1,44 @@
+
+import facebookIcon from "../assets/facebook-icon.svg";
+import twitterIcon from "../assets/twitter-icon.svg";
+import instagramIcon from "../assets/instagram-icon.svg";
+import linkedinIcon from "../assets/linkedin-icon.svg";
+
+import cars from "../assets/cars.svg";
+
+const Footer = () => {
+  return (
+    <div className="py-36">
+      <div
+        className="wrapper flex flex-col lg:flex-row justify-start
+        lg:justify-between gap-12"
+      >
+        <div>
+          <img src={cars} width="100px" height="100px" alt="" />
+          <p className="text-light-gray my-7 text-center lg:text-start">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam,
+            error?
+          </p>
+          <div className="flex items-center gap-[18px] justify-center lg:justify-start">
+            <img src={facebookIcon} alt="" className="cursor-pointer" />
+            <img src={twitterIcon} alt="" className="cursor-pointer" />
+            <img src={instagramIcon} alt="" className="cursor-pointer" />
+            <img src={linkedinIcon} alt="" className="cursor-pointer" />
+            
+          </div>
+        </div>
+
+        <ul className="flex flex-col sm:flex-row items-center gap-10 mx-auto lg:mx-0">
+          <li className="link border-primary-green">Home</li>
+          <li className="link border-transparent">Booking</li>
+          <li className="link border-transparent">About</li>
+          <li className="link border-transparent">Cars</li>
+          <li className="link border-transparent">Services</li>
+          <li className="link border-transparent">Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/views/RentingProcess.jsx
@@ -0,0 +1,44 @@
+import rentingProcessDiagram from "../assets/process.svg";
+import rentingProcessDiagramMobile from "../assets/process-vertical.svg";
+
+import { Step } from "../components";
+
+const steps = [
+  "Choose a vehicle",
+  "Pick location & date",
+  "Book your care",
+  "Finish process",
+];
+
+const RentingProcess = () => {
+  return (
+    <div className="pt-36">
+      <div className="wrapper">
+
+
+      
+         <h1 className=" head_text mb-9 text-center">
+            <span>Steps for the  </span>
+           <span className="text-cyan-300">Process</span>
+          </h1>
+
+        <div className="flex md:flex-col justify-center md:justify-start gap-8">
+          <img src={rentingProcessDiagram} alt="" className="hidden md:block" />
+          <img
+            src={rentingProcessDiagramMobile}
+            alt=""
+            className="block md:hidden"
+          />
+
+          <div className="flex flex-col md:flex-row gap-5">
+            {steps.map((step, i) => (
+              <Step key={i} title={step} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RentingProcess;
